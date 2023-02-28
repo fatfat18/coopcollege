f4c6ec9a9d8ef0b0e9522a09fb17431fc7156a90
PatrickPahunang <patrickcd452@gmail.com>
2023-02-28 12:52:55 +0800

    login with inertia and vuejs

--- coopcollege/resources/js/app.js
+++ coopcollege/resources/js/app.js
@@ -1,12 +1,23 @@
+import './bootstrap';
+import '../css/app.css';
 
-// Require Vue
-window.Vue = require('vue').default;
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-// Register Vue Components
-Vue.component('test', require('./components/test.vue').default);
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-// Initialize Vue
-const app = new Vue({
-    el: '#app',
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
-
