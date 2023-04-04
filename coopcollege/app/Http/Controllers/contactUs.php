aeab66ba2efb6596b2251e1f5032332745dd1987
PatrickPahunang <patrickcd452@gmail.com>
2023-04-04 09:09:40 +0800

    test debug for contactUs controller

--- coopcollege/app/Http/Controllers/contactUs.php
+++ coopcollege/app/Http/Controllers/contactUs.php
@@ -4,11 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+
+//TEST DEBUG - WALAY SULOD ANG REQUEST
 class contactUs extends Controller
 {
+    public function update(Request $request)
+    {
+        $data = $request->all();
 
-    public function update(Request $request){
-        \App\Models\contactUs::where('idcontactUs','1')->update($request->all());
-        return ['msg'=>"Successfuly updated contacts"];
+        if (empty($data)) {
+            return ['msg' => 'No data to update.'];
+        }
+
+        \App\Models\contactUs::where('idcontactUs', 1)->update($data);
+        return ['msg' => 'Successfully updated contact.'];
     }
 }
+
+
+
+
+
+
+//CONTACTUS CONTROLLER NGA GA 500 SERVER ERROR
+
+//class contactUs extends Controller
+//{
+
+//    public function update(Request $request){
+//        \App\Models\contactUs::where('idcontactUs','1')->update($request->all());
+//        return ['msg'=>"Successfuly updated contacts"];
+//    }
+//}
