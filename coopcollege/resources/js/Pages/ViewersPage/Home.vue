9541e026e5a1ad40b63c648affab8429218b23f1
PatrickPahunang <patrickcd452@gmail.com>
2023-04-09 07:52:45 +0800

    minor homepage edits

--- coopcollege/resources/js/Pages/ViewersPage/Home.vue
+++ coopcollege/resources/js/Pages/ViewersPage/Home.vue
@@ -85,12 +85,12 @@ A total of 35 participants coming from 15 co-operatives nationwide joined and ac
 
  
 
-    <div class="card text-xl px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl " data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="100" >
+    <div class="card text-xl px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl " data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="300" >
         <font-awesome-icon icon="fa-solid fa-note-sticky " class="text-theme1 h-10 mb-4" />
         National Federation of Cooperative Primaries in the Philippines -(NaFeCOOP)National Federation of Cooperative Primaries in the Philippines - (NaFeCOOP).
    
     </div>
-    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="300" >
+    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="500" >
         <font-awesome-icon icon="fa-solid fa-coins" class="text-theme1 h-10 mb-4" />
         <span class="text-xl">The NaFeCOOP and its relation to the Co-op College PH:</span><br>
         70% Consumable - for education,
@@ -105,23 +105,23 @@ A total of 35 participants coming from 15 co-operatives nationwide joined and ac
         Education and
         Training Advocacy.<br>
     </div>
-    <div class="card px-4  flex flex-col justify-center text-xs items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl text-center" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="500" >
+    <div class="card px-4  flex flex-col justify-center text-xs items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl text-center" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="700" >
         <font-awesome-icon icon="fa-solid fa-lightbulb" class="text-theme1 h-10 mb-4" />
         <span class="text-xl">The KoopSkwela </span>
         A mobile learning hub and educational initiative by CLIMBS Life and General Insurance Cooperative, the Co-operative College of the Philippines (Co-op College PH), Coop Life General Insurance & Financial Services Agency (CLIFSA), MASS-SPECC Cooperative Development Center, the National Confederation of Cooperatives (NATCCO Network), First Community Cooperative (FICCO), the National Federation of Cooperative Primaries in the Philippines (NaFeCOOP), and with the coordination of the Regional Cooperative Development Council (RCDC) - Region X.
     </div>
-    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="700" >
+    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="900" >
         <font-awesome-icon icon="fa-solid fa-briefcase-medical" class="text-theme1 h-10 mb-4" />
         From the CIFL to the Co-op College PH
 The CLIMBS Institute of Financial Literacy (CIFL) strategically pivoted to the CLIMBS Institute of Management (CIM), and finally to the Co-operative College of the Philippines (Co-op College PH) which aims to further co-operative education by embodying the Co-operative Values & Principles, providing higher level of trainings, upgrade educational & learning platforms, and widen its reach through local & international partnerships. 
     </div>
-    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="900" >
+    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="1100" >
         <font-awesome-icon icon="fa-solid fa-chart-simple" class="text-theme1 h-10 mb-4" />
         <span class="text-xl">Cooperative Management</span><br>
 in partnership with the Asian Institute of
 Management (AIM)
     </div>
-    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="1100" >
+    <div class="card px-4 text-center flex flex-col justify-center items-center my-8 mb-10 h-80 w-96 bg-theme2 mx-8 rounded-xl" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="1300" >
         <font-awesome-icon icon="fa-solid fa-earth-asia" class="text-theme1 h-10 mb-4" />
         <span class="text-xl"> The Global Co-operative Executive Masterclass</span><br>
         Was held in Manchester, United Kingdom, in the heart of the birthplace of the modern Co-operative Movement, following in the footsteps of the Rochdale Pioneers.
